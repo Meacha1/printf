6eff57ad057dae202a08758cab1541c8c9c81a9b
Meacha1 <meachattd@gmail.com>
2022-10-17 14:16:12 -0700

    commit message

--- 0-printf.c
+++ 0-printf.c
@@ -5,26 +5,27 @@
 /**
  * _printf - Write a function that produces output according to a format.
  * @format: array containing conversion specifiers:
- * Return: 0 for success.
+ * Return:  the number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-int i = 0, j, k, count = 0;
+int i = 0, j, k, count2 = 0, count1 = 0, count = 0;
 va_list args;
-
 char *p = NULL;
 char c;
-va_start(args, format);
 
+va_start(args, format);
 for (i = 0; format[i]; i++, count++)
 ;
 for (i = 0; i < count; i++)
 {
 if (format[i] == '%')
 {
+count1++;
 if (format[i + 1] == 'c')
 {
+count2++;
 c = va_arg(args, int);
 putchar(c);
 }
@@ -33,6 +34,7 @@ else if (format[i + 1] == 's')
 p = va_arg(args, char *);
 for (k = 0; p[k]; k++)
 ;
+count2 += k;
 for (j = 0; j < k; j++)
 {
 putchar(p[j]);
@@ -44,7 +46,7 @@ continue;
 if (format[i - 1] == '%')
 continue;
 putchar(format[i]);
-
 va_end(args);
-return (count);
+}
+return (count - count1 * 2 + count2);
 }
